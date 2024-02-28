43a613c5c215d2086e5610072b4b18f760748dfb
Mateus Santos <mateussilvanzg@gmail.com>
2024-02-28 19:58:20 -0300

    Estruturas de repetição

---
+++ aula-13/ambiente.js
@@ -0,0 +1,13 @@
+console.log('Olá, Mundo!')
+
+var num = 1
+while(num <= 10) {
+    console.log(`${num} - Olá, Mundo!`)
+    num++
+}
+
+var pass = 1
+do {
+    console.log(`Passo - ${pass}`)
+    pass++
+} while(pass <= 9)
---
+++ aula-13/node_f68df0e3979a4.tmp.js
@@ -0,0 +1,8 @@
+console.log('Olá, Mundo!<br>')
+
+var num = 0
+var qtd = 10
+while(qtd <= 10) {
+    ++num
+    console.log(`${num} - Olá, Mundo!`)
+}
