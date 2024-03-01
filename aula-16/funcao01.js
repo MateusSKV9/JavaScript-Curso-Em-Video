47a0b1056fc1091cc2dff1241f1be77fdc1258dd
Mateus Santos <mateussilvanzg@gmail.com>
2024-03-01 13:49:22 -0300

    Novas funções

---
+++ aula-16/funcao01.js
@@ -0,0 +1,11 @@
+function parimpar(n) {
+    if(n%2 == 0) {
+        return 'É par!'
+    } else {
+        return 'É ímpar!'
+    }
+}
+
+let num = parimpar(21022)
+console.log(num)
+console.log(parimpar(279))
